3de7ad1903dd9f6e2caf699f68ab791efe6a2343
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 23:21:34 +0700

    update next.config.ts

--- docs/_next/static/3EvliHn_6nLesGCihkue_/_buildManifest.js
+++ docs/_next/static/3EvliHn_6nLesGCihkue_/_buildManifest.js
@@ -1,15 +1,15 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/d1d285e8a6f67069.js"
+    "static/chunks/32298ff2280c96aa.js"
   ],
   "/404": [
-    "static/chunks/525bcd4cb55fc497.js"
+    "static/chunks/a5bb2a0a23b3c383.js"
   ],
   "/500": [
-    "static/chunks/1112646db39bee90.js"
+    "static/chunks/27a1fc71cc9b5a8c.js"
   ],
   "/_error": [
-    "static/chunks/b80aa0f1e648a069.js"
+    "static/chunks/68470e38d4be78f6.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- docs/_next/static/chunks/264ac147a630a705.js
+++ docs/_next/static/chunks/264ac147a630a705.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/58333e22fe8680be.js",
-  "static/chunks/1fbcf9f566cc2c4d.js",
-  "static/chunks/f2eb8f8dcadfaebe.js",
+  "static/chunks/1b8ed19db0a9c26c.js",
+  "static/chunks/81cb2cfb4cf64e20.js",
   "static/chunks/ae3e0be754433a9f.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-7e2729d46ada85cd.js"
+  "static/chunks/turbopack-6e1b91b996215881.js"
 ])
