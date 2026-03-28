8bfa7cda35667be650feaf8ad511ed1fbc75cfbb
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-28 11:45:23 +0700

    Remove mobile

--- docs/_next/static/chunks/0slzmzv-.jeht.js
+++ docs/_next/static/chunks/0slzmzv-.jeht.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/10tiz6t7p-27~.js",
   "static/chunks/1101o4ex-07sb.js",
   "static/chunks/0zq6m5gk5l.~n.js",
-  "static/chunks/01fokk7~reqkr.css",
-  "static/chunks/0eki28umy5xci.css",
-  "static/chunks/turbopack-0klqmxqv--7ko.js"
+  "static/chunks/0lxukkequgh66.css",
+  "static/chunks/0p44cybxe7kuv.css",
+  "static/chunks/turbopack-0v8~ctvptdlt7.js"
 ])
